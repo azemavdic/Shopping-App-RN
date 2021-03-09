eb3655a98a695baf53f393e302e3b1642dd3415e
unknown <asko2212@hotmail.com>
2021-03-09 07:31:48 +0100

    ProductDetail screen layout

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    ScrollView,
+    Image,
+    Button,
+    View,
+} from 'react-native';
 import { useSelector } from 'react-redux';
+import Colors from '../../constants/Colors';
 
 const ProductDetailScreen = (props) => {
     const productId = props.navigation.getParam('productId');
@@ -11,18 +19,52 @@ const ProductDetailScreen = (props) => {
         )
     );
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image
+                source={{ uri: selectedProduct.imageUrl }}
+                style={styles.image}
+            />
+            <View style={styles.actions}>
+                <Button
+                    title='Dodaj u korpu'
+                    onPress={() => {}}
+                    color={Colors.primary}
+                />
+            </View>
+            <Text style={styles.price}>
+                {selectedProduct.price.toFixed(2)} KM
+            </Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     );
 };
 
 ProductDetailScreen.navigationOptions = (navData) => {
     return {
-        headerTitle: navData.navigation.getParam('productTitle')
-    }
+        headerTitle: navData.navigation.getParam('productTitle'),
+    };
 };
 
 export default ProductDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300,
+    },
+    actions:{
+        alignItems:'center',
+        marginVertical:15
+    },
+    price:{
+        fontSize:18,
+        color:'#888',
+        textAlign:'center',
+        marginVertical:20
+    },
+    description:{
+        fontSize:14,
+        textAlign:'center',
+        marginHorizontal:10
+    }
+});
