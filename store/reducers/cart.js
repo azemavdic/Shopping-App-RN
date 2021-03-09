bd5713537f8c49e01edb263799c034ec88415bb0
unknown <asko2212@hotmail.com>
2021-03-09 14:30:24 +0100

    cart starting

---
+++ store/reducers/cart.js
@@ -0,0 +1,14 @@
+import { ADD_TO_CART } from "../actions/cart";
+
+const initialState = {
+    items: [],
+    totalAmount: 0,
+};
+
+export default (state = initialState, action) => {
+    switch(action.type){
+        case ADD_TO_CART:
+            
+    }
+    return state;
+};
