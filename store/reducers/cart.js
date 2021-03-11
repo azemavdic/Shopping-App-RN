d718d5c294c57bc5aed28245e1312dcd5bdb220f
unknown <asko2212@hotmail.com>
2021-03-11 09:12:36 +0100

    clear cart

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,5 +1,6 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cartItem";
+import { ADD_ORDER } from "../actions/orders";
 
 const initialState = {
   items: {},
@@ -60,6 +61,8 @@ export default (state = initialState, action) => {
         items: updatedCartItems,
         totalAmount: state.totalAmount - selectedCartItem.productPrice
       }
+      case ADD_ORDER:
+        return initialState
   }
   return state;
 };
